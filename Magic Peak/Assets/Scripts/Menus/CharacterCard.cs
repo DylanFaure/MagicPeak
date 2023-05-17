a4800459d3869b50a5b2000b35bf1fa12c50701d
DylanFaure <dylan1.faure@epitech.eu>
2023-05-17 08:07:54 +0200

    [ADD] Show or Hide character name and color when unlock or not

---
+++ Magic Peak/Assets/Scripts/Menus/CharacterCard.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CharacterCard : MonoBehaviour
+{
+    [SerializeField] private Image characterImage;
+    [SerializeField] private TextMeshProUGUI characterName;
+
+    void Awake()
+    {
+        characterImage = this.transform.Find("CharacterSprite").GetComponent<Image>();
+        characterName = this.transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerPrefs.GetInt(gameObject.name) == 0)
+        {
+            characterImage.color = new Color(0f, 0f, 0f, 1f);
+            characterName.text = "???";
+        }
+        else
+        {
+            characterImage.color = new Color(1f, 1f, 1f, 1f);
+            characterName.text = gameObject.name;
+        }
+    }
+}
